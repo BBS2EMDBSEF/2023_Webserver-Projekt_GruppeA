610e1ca3e90cce793ec47a0dfcb3dabf1aec440e
DerPizzaBursche <nick.roelfs@t-online.de>
2023-12-19 19:39:21 +0100

    install update zum testing

--- Skripte/sammlungV2.1/install.sh
+++ Skripte/sammlungV2.1/install.sh
@@ -7,20 +7,19 @@ timedatectl set-timezone Europe/Berlin
 sudo apt install wget
 sudo mkdir /usr/share/dotnet
 if getconf LONG_BIT == 32; then
-    wget sudo tar zxf dotnet-sdk-6.0.417-linux-arm64.tar.gz -C /usr/share/dotnet/;
+    wget https://download.visualstudio.microsoft.com/download/pr/a72dea03-21fd-48c6-bf0c-78e621b60514/e0b8f186730fce858eb1bffc83c9e41c/dotnet-sdk-6.0.417-linux-arm.tar.gz
     sudo tar zxf dotnet-sdk-6.0.417-linux-arm.tar.gz -C /usr/share/dotnet/
 else
     wget https://download.visualstudio.microsoft.com/download/pr/03972b46-ddcd-4529-b8e0-df5c1264cd98/285a1f545020e3ddc47d15cf95ca7a33/dotnet-sdk-6.0.417-linux-arm64.tar.gz;
     sudo tar zxf dotnet-sdk-6.0.417-linux-arm64.tar.gz -C /usr/share/dotnet/
 fi
 
-echo 'export PATH=$PATH:/usr/share/dotnet' | sudo tee -a ~/.profile
-echo 'export DOTNET_ROOT=/usr/share/dotnet' | sudo tee -a ~/.profile
+sudo echo 'export PATH=$PATH:/usr/share/dotnet' >> ~/.profile
+sudo echo 'export DOTNET_ROOT=/usr/share/dotnet' >> ~/.profile
+sudo cat ~/.profile
 
-
-@reboot continueInstall.sh
-echo "@reboot continueInstall.sh" | sudo crontab -
-sudo chmod +x continueInstall.sh
+sudo nano ~/.bashrc
+sudo sh /home/github/sammlungV2.1/continueInstall.sh
 sudo reboot -h now
 
 

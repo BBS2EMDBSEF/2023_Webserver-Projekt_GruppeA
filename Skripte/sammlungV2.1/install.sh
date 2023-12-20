6c7f2c59a4df43d6e578d7d20eec125e5a68d09d
DerPizzaBursche <nick.roelfs@t-online.de>
2023-12-20 08:38:27 +0100

    anpassung install script
    neuer Skript test
    
    edited Skript for Testing
    
    Skript edited for testing
    
    anpassung für ssh und mysql
    
    Skript angepasst + 32bit und 64bit build erstellt
    
    Skript edited for testing
    
    Ordner gezipt
    
    abfragen des aktuellen Users angepasst
    
    Dateipfad Korrigiert?

--- Skripte/sammlungV2.1/install.sh
+++ Skripte/sammlungV2.1/install.sh
@@ -1,56 +1,77 @@
 #!/bin/bash
 
-apt update
-sudo apt install raspberrypi-ui-mods
+#user=$SUDO_USER
+apt update -y
+sudo apt install raspberrypi-ui-mods -y
 update-locale LANG=de_DE.UTF-8 LC_MESSAGES=POSIX
 timedatectl set-timezone Europe/Berlin
-sudo apt install wget
+sudo apt install wget -y
 sudo mkdir /usr/share/dotnet
+sudo mkdir -p /home/$SUDO_USER/backend
+echo $SUDO_USER
 if getconf LONG_BIT == 32; then
     wget https://download.visualstudio.microsoft.com/download/pr/a72dea03-21fd-48c6-bf0c-78e621b60514/e0b8f186730fce858eb1bffc83c9e41c/dotnet-sdk-6.0.417-linux-arm.tar.gz
     sudo tar zxf dotnet-sdk-6.0.417-linux-arm.tar.gz -C /usr/share/dotnet/
+    #sudo cp -r 2023_Webserver-Projekt_GruppeA/Skripte/sammlungV2.1/App32Bit/* /home/$SUDO_USER/backend/
+    sudo unzip "/home/$SUDO_USER/2023_Webserver-Projekt_GruppeA/Skripte/sammlungV2.1/App32Bit.zip" -d "/home/$SUDO_USER/backend" && mv "/home/$SUDO_USER/backend/App32Bit/*" "/home/$SUDO_USER/backend"
+    sudo rm -r /home/$SUDO_USER/backend/App32Bit
 else
     wget https://download.visualstudio.microsoft.com/download/pr/03972b46-ddcd-4529-b8e0-df5c1264cd98/285a1f545020e3ddc47d15cf95ca7a33/dotnet-sdk-6.0.417-linux-arm64.tar.gz;
     sudo tar zxf dotnet-sdk-6.0.417-linux-arm64.tar.gz -C /usr/share/dotnet/
+    #sudo cp -r 2023_Webserver-Projekt_GruppeA/Skripte/sammlungV2.1/App64Bit/* /home/$SUDO_USER/backend/
+    sudo unzip "/home/$SUDO_USER/2023_Webserver-Projekt_GruppeA/Skripte/sammlungV2.1/App64Bit.zip" -d "/home/$SUDO_USER/backend" && mv "/home/$SUDO_USER/backend/App64Bit/*" "/home/$SUDO_USER/backend"
+    sudo rm -r /home/$SUDO_USER/backend/App64Bit
 fi
 
-#cat << EOF >> ~/.profile
-sudo tee >> ~/.profile <<EOF
-if [ -n "\$BASH_VERSION" ]; then
-    if [ -f "\$HOME/.bashrc" ]; then
-        . "\$HOME/.bashrc"
+
+code="\nif [ -n \"\$BASH_VERSION\" ]; then
+    if [ -f \"\$HOME/.bashrc\" ]; then
+        . \"\$HOME/.bashrc\"
     fi
 fi
-if [ -d "\$HOME/bin" ] ; then
-    PATH="\$HOME/bin:\$PATH"
+if [ -d \"\$HOME/bin\" ] ; then
+    PATH=\"\$HOME/bin:\$PATH\"
 fi
-if [ -d "\$HOME/.local/bin" ] ; then
-    PATH="\$HOME/.local/bin:\$PATH"
+if [ -d \"\$HOME/.local/bin\" ] ; then
+    PATH=\"\$HOME/.local/bin:\$PATH\"
 fi
 export PATH=\$PATH:/usr/share/dotnet
-export DOTNET_ROOT=/usr/share/dotnet
-EOF
+export DOTNET_ROOT=/usr/share/dotnet"
+
+
+for user_home in /home/*; do
+    profile_file="$user_home/.profile"
+    if [ -f "$profile_file" ]; then
+        echo -e "$code" >> "$profile_file"
+    else
+        echo "[FAILED] -  no .profile file found ==> Skipping user: $(basename $user_home) "
+    fi
+done
 
 apt install -y openssh-server
 
 apt install -y nginx
-sudo apt update
+#sudo apt update
 sudo apt install -y apt-transport-https lsb-release ca-certificates
 sudo apt install -y lsb-release apt-transport-https ca-certificates
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-sudo apt install php8.1 php8.1-fpm php8.1-mysql php8.1-xml php8.1-curl php8.1-gd -y
+sudo apt install php8.1-fpm php8.1-mysql php8.1-xml php8.1-curl php8.1-gd -y
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
 
-
+MYSQL_ROOT_PASSWORD = "schule";
 sudo apt install -y mariadb-server
-echo "mysql-server mysql-server/root_password password schule" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password schule" | sudo debconf-set-selections
-mysql -u root -p$github-e "CREATE USER 'Service'@'localhost' IDENTIFIED BY 'Emden123';"
-mysql -u root -p$github -e "GRANT ALL PRIVILEGES ON *.* TO 'Service'@'localhost' WITH GRANT OPTION;"
-mysql -u root -p$github -e "FLUSH PRIVILEGES;"
-
-
+echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE projektgruppea;" 
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'Service'@'localhost' IDENTIFIED BY 'Emden123';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'Service'@'localhost' WITH GRANT OPTION;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+# sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';"
+# sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+# sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='';"
+# sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+# sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 sudo tee /etc/nginx/sites-available/default <<EOF
 server {
@@ -87,6 +108,7 @@ location /phpmyadmin {
 }
 EOF
 
+
 sudo tee /etc/systemd/system/backend.service <<EOF
 #backend.service -File
 #Dir: /etc/systemd/system/
@@ -95,8 +117,8 @@ sudo tee /etc/systemd/system/backend.service <<EOF
 Description=Projekt Backend
 
 [Service]
-WorkingDirectory=/home/github/backend/
-ExecStart=/usr/bin/dotnet /home/github/backend/ServerAppSchule.dll --urls http://0.0.0.0:5000
+WorkingDirectory=/home/$SUDO_USER/backend/
+ExecStart=/usr/bin/dotnet /home/$SUDO_USER/backend/ServerAppSchule.dll --urls http://0.0.0.0:5000
 Restart=always
 # Restart service after 10 seconds if the dotnet service crashes:
 RestartSec=10
@@ -120,19 +142,27 @@ sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
 sudo ufw allow 'Nginx Full'
 sudo ufw allow mysql
-sudo ufw enable <<< 'y'
+sudo ufw enable -y
 
-# Aktiviere die Firewall
 ufw enable
 
 systemctl enable backend 
+systemctl enable ssh
+systemctl enable nginx
+systemctl enable mysql
+
+systemctl start ssh
 systemctl start backend
 systemctl start nginx
 systemctl start mysql
-systemctl enable nginx
-systemctl enable mysql
+
 systemctl status nginx
 systemctl status mysql
+systemctl status ssh
+systemctl start backend
 
-#echo 'sudo sh /home/github/sammlungV2.1/continueInstall.sh' >> ~/.bashrc
+sudo apt-get purge apache2 apache2-utils apache2.2-bin apache2-common -y
+sudo rm -rf /etc/apache2/ -y
+sudo rm -rf /var/log/apache2/ -y
+
 sudo reboot -h now
